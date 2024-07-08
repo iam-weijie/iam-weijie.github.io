2b1a392730c496cfc96cfa9796e8a624c7c9af99
Weijie Wang <wang.weijie0110@gmail.com>
2024-07-08 14:15:10 -0400

    prettier format change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.scss';
-import Layout from './components/Layout';
+import { Route, Routes } from 'react-router-dom'
+import './App.scss'
+import Layout from './components/Layout'
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element = {<Layout />}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,7 @@
-import './index.scss';
+import './index.scss'
 
 const Layout = () => {
-    return <>Hello</>
+  return <>Hello</>
 }
 
 export default Layout
