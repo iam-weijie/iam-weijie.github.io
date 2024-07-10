9827842faf2d9fd29bff46ba1c4ec843707977c0
Weijie Wang <wang.weijie0110@gmail.com>
2024-07-10 17:32:20 -0400

    js animation for projects section

--- script.js
+++ script.js
@@ -9,6 +9,61 @@ var typed = new Typed(".typing", {
 // Sidebar
 let sidebar = document.querySelector(".sidebar");
 
+// Slider
+let items = document.querySelectorAll(".projects .slider .list .item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let thumbnails = document.querySelectorAll(".projects .thumbnail .item");
+// Config param
+let countItem = items.length;
+let itemActive = 0;
+// Event next click
+next.onclick = function () {
+  itemActive = itemActive + 1;
+  if (itemActive >= countItem) {
+    itemActive = 0;
+  }
+  showSlider();
+};
+//event prev click
+prev.onclick = function () {
+  itemActive = itemActive - 1;
+  if (itemActive < 0) {
+    itemActive = countItem - 1;
+  }
+  showSlider();
+};
+// Auto run slider
+let refreshInterval = setInterval(() => {
+  next.click();
+}, 5000);
+function showSlider() {
+  // remove item active old
+  let itemActiveOld = document.querySelector(
+    ".projects .slider .list .item.active"
+  );
+  let thumbnailActiveOld = document.querySelector(
+    ".projects .thumbnail .item.active"
+  );
+  itemActiveOld.classList.remove("active");
+  thumbnailActiveOld.classList.remove("active");
+  // active new item
+  items[itemActive].classList.add("active");
+  thumbnails[itemActive].classList.add("active");
+  // clear auto time run slider
+  clearInterval(refreshInterval);
+  refreshInterval = setInterval(() => {
+    next.click();
+  }, 5000);
+}
+// Click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener("click", () => {
+    itemActive = index;
+    showSlider();
+  });
+});
+
 // Contact form
 const contactForm = document.querySelector("#contact-form");
 const submitBtn = document.querySelector(".submit-btn");
